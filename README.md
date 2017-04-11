8f08536ca2bc62dc4fa081345b2ad69202e565b0
Rafał Gierczak <grime85@gmail.com>
2017-04-12 00:09:16 +0200

    Added flex package

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # test-angular-flex-layout
+
+> The test application for angular flex layout
+
+## Installation
+
+```
+$ npm install
+```
+
+## Usage
+
+```
+$ npm run start
+```
+
+Run the project in your browser
+
+```
+localhost:4200
+```
+
+## Flex package size
+
+The bundled flex package size is 101 kB.
+
+![](https://github.com/rgierczak/starter-angular-webpack/blob/master/src/assets/flex-layout-size.png)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FlexLayoutModule
     ],
     providers: [],
     bootstrap: [AppComponent]
